5229c4d4b0fe15f72d83ed084e763a0e978551f4
Radji Mohamed <radjimohamed013@gmail.com>
2025-02-21 21:20:01 +0100

    creation de la bd lors de la premiere requete

--- app.py
+++ app.py
@@ -22,6 +22,11 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
 db.init_app(app)
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+    
 migrate = Migrate(app, db)
 
 #fake = Faker()
