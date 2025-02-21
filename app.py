bc2c73ee7920e29c17324fdd6d6e95ae9eff6165
Radji Mohamed <radjimohamed013@gmail.com>
2025-02-21 21:37:31 +0100

    supression de db.create_all()

--- app.py
+++ app.py
@@ -22,9 +22,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
 db.init_app(app)
-@app.before_first_request
-def create_tables():
-    db.create_all()
+
 
     
 migrate = Migrate(app, db)
